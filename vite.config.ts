02028795aa093a27aab5bb1fbec36c684b86d632
Raul 'Station' Perez <raulytstation@outlook.com>
2022-12-13 10:45:09 -0400

    revert: Reverting Previous Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
-import vercel from "solid-start-vercel";
 
 export default defineConfig({
-  plugins: [solid({ ssr: false, adapter: vercel() })],
+    plugins: [solid({ ssr: false })],
 });
