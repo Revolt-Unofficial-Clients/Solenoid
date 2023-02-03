b1f2279e554a25276db39de0ee036cc574d5814c
StationaryStation <raulytstation@outlook.com>
2023-02-03 18:25:30 -0400

    feat: Add tab functionality

--- src/components/ui/settings/index.tsx
+++ src/components/ui/settings/index.tsx
@@ -1,4 +1,9 @@
-import { Component } from "solid-js";
+import { Component, createSignal, Match, Switch } from "solid-js";
+import * as tabs from "./tabs"
+
+// Tabs start at index 0.
+// Settings should start at the user profile screen.
+const [currentTab, setCurrentTab] = createSignal<number>(0);
 
 const Settings: Component = () => {
   return (
@@ -6,7 +11,28 @@ const Settings: Component = () => {
       class="absolute z-20 w-full h-full overflow-scroll overflow-x-hidden bg-base-300"
       id="solenoid-settings-panel"
     >
-      Settings
+      <Switch>
+        <Match when={currentTab() === 0}>
+          <tabs.userProfileModule />
+        </Match>
+        <Match when={currentTab() === 1}>
+          <tabs.serverIdentityModule />
+        </Match>
+        <Match when={currentTab() === 2}>
+          <tabs.clientModule />
+        </Match>
+        <Match when={currentTab() === 3}>
+          <tabs.experimentsModule />
+        </Match>
+        <Match when={currentTab() === 4}>
+          <tabs.aboutModule />
+        </Match>
+        <Match when={currentTab() === 5}>
+          <div class="text-3xl font-heading font-black">
+            Tab not found ;w;
+          </div>
+        </Match>
+      </Switch>
     </div>
   );
 };
--- src/components/ui/settings/tabs/index.ts
+++ src/components/ui/settings/tabs/index.ts
@@ -1,6 +1,7 @@
 import aboutModule from "./about";
 import clientModule from "./client";
+import userProfileModule from "./user";
 import experimentsModule from "./experiments";
 import serverIdentityModule from "./identity";
 
-export { aboutModule, clientModule, experimentsModule, serverIdentityModule }
+export { aboutModule, clientModule, experimentsModule, serverIdentityModule, userProfileModule }
