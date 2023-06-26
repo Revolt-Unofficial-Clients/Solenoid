f973c7de87512f15a3e6e00e344bdda6733fcfe4
Amy <raulytstation@outlook.com>
2023-06-25 20:52:47 -0400

    chore: Format channels file

--- src/components/ui/navigation/navbar/channels.tsx
+++ src/components/ui/navigation/navbar/channels.tsx
@@ -1,44 +1,49 @@
 import classNames from "classnames";
-import { Component, For, Show } from "solid-js";
-import { className } from "solid-js/web/types";
+import { Component, For } from "solid-js";
 import * as Solenoid from "../../../../lib/solenoid";
 import { Markdown } from "../../../markdown";
 
 async function getMessagesFromChannel() {
-  await Solenoid.servers.current_channel
-    ?.messages.fetchMultiple({"include_users": true})
-    .then(messages =>
-      Solenoid.setMessages(messages.reverse())
-    );
+  await Solenoid.servers.current_channel?.messages
+    .fetchMultiple({ include_users: true })
+    .then((messages) => Solenoid.setMessages(messages.reverse()));
   Solenoid.setServers("isHome", false);
 }
 
 const ChannelNavigation: Component = () => {
-
   return (
     <div class="relative bottom-0 left-0 container w-96 h-screen bg-base-200 px-4 overflow-scroll overflow-x-hidden">
       <div class="prose py-2">
         <h2>{Solenoid.servers.current_server?.name}</h2>
       </div>
       <For each={Solenoid.servers.current_server?.orderedChannels}>
-        {category => (
+        {(category) => (
           <div class="flex flex-col gap-1">
             <p class="font-semibold m-2">{category.name}</p>
             <For each={category.channels}>
-              {channel => (
-                <button class={classNames({
-                  "w-full": true,
-                  "h-auto": true,
-                  "bg-neutral": true,
-                  "items-center": true,
-                  "p-[0.35rem]": true,
-                  "flex": true,
-                  "gap-2": true,
-                  "btn-primary": channel.id === Solenoid.servers.current_channel?.id
-                })} onClick={() => {
-                  Solenoid.setServers("current_channel", channel)
-                  getMessagesFromChannel()
-                }}><Markdown content={channel.name} /> {channel.unread && <div class="w-2 h-2 bg-white ml-auto mr-2 rounded-full"></div>}</button>
+              {(channel) => (
+                <button
+                  class={classNames({
+                    "w-full": true,
+                    "h-auto": true,
+                    "bg-neutral": true,
+                    "items-center": true,
+                    "p-[0.35rem]": true,
+                    flex: true,
+                    "gap-2": true,
+                    "btn-primary":
+                      channel.id === Solenoid.servers.current_channel?.id,
+                  })}
+                  onClick={() => {
+                    Solenoid.setServers("current_channel", channel);
+                    getMessagesFromChannel();
+                  }}
+                >
+                  <Markdown content={channel.name} />{" "}
+                  {channel.unread && (
+                    <div class="w-2 h-2 bg-white ml-auto mr-2 rounded-full"></div>
+                  )}
+                </button>
               )}
             </For>
           </div>
--- src/components/ui/navigation/navbar/servers.tsx
+++ src/components/ui/navigation/navbar/servers.tsx
@@ -1,66 +1,104 @@
 import { Component, Match, Switch } from "solid-js";
-import type { Server } from "revolt-toolset";
+import type { Server } from "revkit";
 import { createSignal, For } from "solid-js";
 import { revolt } from "../../../../lib/revolt";
-import { setServers, servers } from "../../../../lib/solenoid";
-import classNames from "classnames";
-import { BiSolidHome } from "solid-icons/bi"
+import {
+  setServers,
+  servers,
+  setMessages,
+  setSettings,
+} from "../../../../lib/solenoid";
+import { BiSolidCog } from "solid-icons/bi";
 
 const [serverlist, setServerList] = createSignal<Server[]>([]);
 
+async function showSettingsModal() {
+  const userinfo = await revolt.api.get("/users/@me");
+  setSettings("statusText", userinfo.status?.text);
+  setSettings("status", userinfo.status?.presence);
+  setSettings("show", true);
+}
+
 const Navigation: Component = () => {
+  const goHome = () => {
+    setServers("current_server", undefined);
+    setServers("current_channel", undefined);
+    setMessages([]);
+    setServers("isHome", true);
+  };
+
+  const setServer = (server: Server) => {
+    setServers("current_server", server);
+
+    // Go to the first found channel
+    setServers("current_channel", undefined);
+    setServers("isHome", false);
+  };
+
   setServerList(revolt.servers.items());
   return (
-    <div class="flex flex-col h-screen bg-base-300 px-4">
-      <button
-        onClick={() => {
-          setServers("current_server", undefined);
-          setServers("current_channel", undefined);
-          setServers("isHome", true);
-        }}
-        class={classNames({
-          btn: true,
-          "btn-active": servers.isHome,
-          "my-2": true,
-          "w-full": true
-        })}
-      >
-        <BiSolidHome />
-      </button>
+    <div class="flex flex-col shrink-0 h-screen bg-base-300 overflow-y-scroll">
+      <div class="indicator m-4 self-center">
+        <span
+          class="indicator-item badge w-2"
+          classList={{
+            "badge-success": revolt.user.presence === "Online",
+            "badge-info": revolt.user.presence === "Focus",
+            "badge-warning": revolt.user.presence === "Idle",
+            "badge-content": revolt.user.presence === "Invisible",
+          }}
+        />
+        <button
+          onClick={goHome}
+          class="grid place-items-center avatar self-center"
+        >
+          <div
+            class="w-12 rounded-full"
+            classList={{
+              "ring ring-primary ring-offset-base-100 ring-offset-2":
+                servers.isHome,
+            }}
+          >
+            <img src={revolt.user.generateAvatarURL({ max_side: 256 })} />
+          </div>
+        </button>
+      </div>
       <For each={serverlist()}>
         {(server) => (
           <button
-            class={
-              classNames({
-                btn: true,
-                "btn-active": servers.current_server === server,
-                "my-2": true
-              })
-            }
+            class="my-2 mx-4 self-center"
             onClick={() => {
-              setServers("current_server", server);
-              setServers("isHome", false);
+              setServer(server);
             }}
           >
             <Switch>
               <Match when={server.icon}>
-                <div class="avatar" onClick={() => {
-                  setServers("current_server", server);
-                  setServers("current_channel", undefined);
-                  setServers("isHome", false);
-                }}>
-                  <div class="w-12 h-12 rounded-full">
+                <div class="avatar">
+                  <div
+                    class="w-12 rounded-full"
+                    classList={{
+                      "ring ring-offset-base-100 ring-offset-2":
+                        servers.current_server?.id === server.id,
+                    }}
+                  >
                     <img src={server.generateIconURL()} />
                   </div>
                 </div>
               </Match>
               <Match when={!server.icon}>
-                <div class="avatar placeholder" onClick={() => {
-                  setServers("current_server", server);
-                  setServers("current_channel", undefined);
-                  setServers("isHome", false);
-                }}>
-                  <div class="w-12 h-12 bg-neutral-focus font-bold rounded-full">
+                <div
+                  class="avatar placeholder"
+                  onClick={() => {
+                    setServer(server);
+                  }}
+                >
+                  <div
+                    class="w-12 h-12 bg-neutral-focus font-bold rounded-full"
+                    classList={{
+                      "ring ring-offset-base-100 ring-offset-2":
+                        servers.current_server?.id === server.id,
+                    }}
+                  >
                     <span>{server.name[0]}</span>
                   </div>
                 </div>
@@ -69,6 +107,16 @@ const Navigation: Component = () => {
           </button>
         )}
       </For>
+      <div
+        class="avatar placeholder self-center mb-2 cursor-pointer"
+        onClick={showSettingsModal}
+      >
+        <div class="w-12 h-12 bg-neutral-focus font-bold rounded-full">
+          <span>
+            <BiSolidCog class="fill-white w-6 h-6" />
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
