1d0c83c481e64c39568166d4f47d06f2b7a7c0ee
Amy <raulytstation@outlook.com>
2023-06-25 20:51:27 -0400

    fix: messages having the wrong order

--- src/components/ui/messaging/Message/Container.tsx
+++ src/components/ui/messaging/Message/Container.tsx
@@ -6,33 +6,33 @@ import type { Component } from "solid-js";
 import { revolt } from "../../../../lib/revolt";
 import { SystemMessageBase } from "./SystemBase";
 
-revolt.on("message", async m => {
-    if (m.channelID !== servers.current_channel?.id) return;
-    setMessages((old) => [...old, m])
-})
+revolt.on("message", async (m) => {
+  if (m.channelID !== servers.current_channel?.id) return;
+  setMessages((old) => [...old, m]);
+});
 
 const MessageContainer: Component = () => {
-    return (
-        <For each={messages?.reverse()}>
-            {message => {
-                if (message?.isSystem()) {
-                    return (
-                    <div>
-                        <p> System Message id {message.id}</p>
-                        <SystemMessageBase sysmessage={message} />
-                    </div>
-                )
-                } else if (message?.isUser()) {
-                    return (
-                        <div>
-                            <UserMessageBase message={message} />
-                        </div>
-                    )
-                }
-            }}
-        </For>
-    )
-}
+
+  return (
+    <For each={messages} fallback={<p>No items</p>}>
+      {(message) => {
+        if (message?.isSystem()) {
+          return (
+            <div>
+              <p> System Message id {message.id}</p>
+              <SystemMessageBase sysmessage={message} />
+            </div>
+          );
+        } else if (message?.isUser()) {
+          return (
+            <div>
+              <UserMessageBase message={message} />
+            </div>
+          );
+        }
+      }}
+    </For>
+  );
+};
 
 export { MessageContainer };
-
